63e9ae6988544d1ada20ecbea165e6f45984c586
Aashutosh Pudasaini <nepalidude3@gmail.com>
2025-01-02 21:59:37 +0545

    add: linear search

--- lab1/bubble.c
+++ lab1/bubble.c
@@ -2,12 +2,12 @@
 
 void bubble_sort(int data[], const int size) {
   for (int i = 0; i < size - 1; i++) {
-  int changed = 0;
-    for (int j = 0; j < size - i-1; j++) {
-      if (data[j] > data[j+1]) {
+    int changed = 0;
+    for (int j = 0; j < size - i - 1; j++) {
+      if (data[j] > data[j + 1]) {
         const int temp = data[j];
-        data[j] = data[j+1];
-        data[j+1] = temp;
+        data[j] = data[j + 1];
+        data[j + 1] = temp;
         changed = 1;
       }
     }
@@ -19,7 +19,7 @@ void bubble_sort(int data[], const int size) {
 }
 
 int main(void) {
-  const int data[] = {41,2,1,34,5,3,45,2,3,422,9};
+  const int data[] = {41, 2, 1, 34, 5, 3, 45, 2, 3, 422, 9};
   const int data_length = sizeof(data) / sizeof(data[0]);
 
   printf("unsorted: ");
---
+++ lab1/linear_search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int linear_search(const int numbers[], const int numbers_length, const int element);
+
+int main(void) {
+  const int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
+
+  const int numbers_length = sizeof(numbers) / sizeof(numbers[0]);
+
+  const int to_find = 3;
+
+  const int index = linear_search(numbers, numbers_length, to_find);
+
+  if (index != -1) {
+    printf("Found Element %d at index %d \n", to_find, index);
+  } else {
+    printf("Not Found Element %d \n", to_find);
+  }
+}
+
+int linear_search(const int numbers[], const int numbers_length, const int element) {
+  int found_index = -1;
+
+  for (int i = 0; i < numbers_length; i++) {
+    if (numbers[i] == element) {
+      found_index = i;
+      break;
+    }
+  }
+
+  return found_index;
+}
