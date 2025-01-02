494186b9bf023e01a074c9fa23e3be86105fc070
Aashutosh Pudasaini <nepalidude3@gmail.com>
2025-01-02 21:45:10 +0545

    standardized the solution

--- lab1/bubble.c
+++ lab1/bubble.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 
 void bubble_sort(int data[], const int size) {
-  int changed = 0;
   for (int i = 0; i < size - 1; i++) {
-    for (int j = i; j < size; j++) {
-      if (data[i] > data[j]) {
-        const int temp = data[i];
-        data[i] = data[j];
-        data[j] = temp;
+  int changed = 0;
+    for (int j = 0; j < size - i-1; j++) {
+      if (data[j] > data[j+1]) {
+        const int temp = data[j];
+        data[j] = data[j+1];
+        data[j+1] = temp;
         changed = 1;
       }
     }
     if (!changed) {
+      printf("=> O(n) condition\n");
       break;
     }
   }
 }
 
 int main(void) {
-  puts("hello world!\n");
-  const int data[] = {21,3,4,6,12,5,33,1};
+  const int data[] = {41,2,1,34,5,3,45,2,3,422,9};
   const int data_length = sizeof(data) / sizeof(data[0]);
 
-  printf("unsorted ");
+  printf("unsorted: ");
   for (int i = 0; i < data_length; i++) {
     printf("%d ", data[i]);
   }
   printf("\n");
 
-  bubble_sort(&data, data_length);
+  bubble_sort(data, data_length);
 
-  printf("sorted ");
+  printf("sorted: ");
   for (int i = 0; i < data_length; i++) {
     printf("%d ", data[i]);
   }
   printf("\n");
+
+  puts("Code ran by Aashutosh Pudasaini!");
 }
