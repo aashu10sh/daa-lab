4309b98cb31ccc0e404cfcbef03d2fdb9531c29c
Aashutosh Pudasaini <nepalidude3@gmail.com>
2025-01-15 23:00:26 +0545

    random

--- lab1/insertion.c
+++ lab1/insertion.c
@@ -3,7 +3,15 @@
 
 
 int main(void){
-    printf("Insertion Sort!");
+
+  const int data[] = {41, 2, 1, 34, 5, 3, 45, 2, 3, 422, 9};
+  
+  const int data_length = sizeof(data) / sizeof(data[0]);
+
+  printf("unsorted: ");
+  print_array(data, data_length);
+  
+  printf("Insertion Sort!\n");
 
 
     print_credit();
